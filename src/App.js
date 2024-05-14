0eb022f698de1deb7f44c5b813184166bd0413dc
im_subhadeep <mondal.sdeep09@gmail.com>
2024-05-15 03:53:28 +0530

    adding frontend part

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './Styles/App.css';
+import Login from './Components/Login';
+import DataContextProvider from './Components/DataContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Room from './Room/Room';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    document.querySelector("#change-theme").addEventListener('click', () => {
+      document.querySelector("#root").classList.toggle("dark");
+      if (document.querySelector("#root").classList.contains("dark")) {
+        const colors = document.querySelectorAll(".colors .color");
+        if (colors) colors[0].style.setProperty('--color', '#fff');
+      } else {
+        const colors = document.querySelectorAll(".colors .color");
+        if (colors) colors[0].style.setProperty('--color', '#000');
+      }
+    });
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataContextProvider>
+      <div className="App">
+        <div id="change-theme">
+          <button id="change-theme-button">
+            <i className="fas fa-moon"></i>
+            <i className="fas fa-sun"></i>
+          </button>
+        </div>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/room" element={<Room />} />
+          </Routes>
+        </Router>
+      </div>
+    </DataContextProvider >
   );
 }
 
